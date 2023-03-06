8a9a8276e7e6737dadb59e03140d0cd705e0a20e
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-06 18:25:42 +0700

    complete make service cards
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 
 import { Container,Row,Col } from 'reactstrap';
+import serviceData from '../assets/data/serviceData';
 import {motion} from 'framer-motion';
 import './services.css';
 const Services = () => {
   return <section className="services">
     <Container>
         <Row>
-       <Col lg="3" md='4'>
-        <div className='service__item'>
+            {
+                serviceData.map((item,index)=>(
+                    <Col lg="3" md='4' key={index}>
+        <motion.div whileHover={{scale:1.1}}className='service__item' style={{
+            background:`${item.bg}`
+        }}>
             <span>
-        <i class="ri-truck-line"></i>
+        <i class={item.icon}></i>
           </span>
          <div>
-        <h3>Free Shipping</h3>
-        <p>Helloo wlcome s fnnugdsvd</p>
+        <h3>{item.title}</h3>
+        <p>{item.subtitle}</p>
     </div>
-    </div>
+    </motion.div>
             </Col>
+                ))
+            }
         </Row>
     </Container>
   </section>
