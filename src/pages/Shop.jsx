854abe3eff4c647647314cbaedeb78805cd6b4d3
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-07 21:43:33 +0700

    create product details page
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -42,7 +42,7 @@ const Shop = () => {
     <section>
       <Container>
         <Row>
-          <Col lg='3' md='3'>
+          <Col lg='3' md='6'>
             <div className="filter__widget">
               <select onClick={handerFilter}>
                 <option>Filter By Category</option>
@@ -54,7 +54,7 @@ const Shop = () => {
               </select>
             </div>
           </Col>
-          <Col lg='3' md='3'>
+          <Col lg='3' md='6' className='text-end'>
           <div className="filter__widget">
               <select >
               <option>Sort By</option>
@@ -64,7 +64,7 @@ const Shop = () => {
             </div>
           </Col>
           
-          <Col lg='6' md='6'>
+          <Col lg='6' md='12'>
             <div className="search__box">
               <input type="text" 
               placeholder='Search...' 
