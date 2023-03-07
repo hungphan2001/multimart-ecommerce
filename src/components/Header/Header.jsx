cc840500ef3c647c5a9bc144267b0d064dcfdc9f
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-07 13:29:46 +0700

    create add product to cart
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/images/eco-logo.png'
 import userIcon from '../../assets/images/user-icon.png'
 import { NavLink } from 'react-router-dom';
 import { Container,Row } from 'reactstrap';
-
+import { useSelector } from 'react-redux';
 const nav__links =[
     {
     path:'home',
@@ -24,6 +24,7 @@ const nav__links =[
 const Header = () => {
 
     const headerRef = useRef(null);
+    const totalQuantity = useSelector(state=>state.cart.totalQuantity)
     const menuRef = useRef(null)
 
     const stickyHeaderFunc =()=>{
@@ -73,7 +74,7 @@ const Header = () => {
                           </span>
                           <span className="cart__icon">
                           <i class="ri-shopping-bag-line"></i>
-                          <span className="badge">1</span>
+                          <span className="badge">{totalQuantity}</span>
                           </span>
                           <span>
                             <motion.img src={userIcon}  whileTap={{scale:1.2}} alt=''></motion.img></span>
--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -5,7 +5,22 @@ import productImg from '../../assets/images/arm-chair-01.jpg'
 import { Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import products from '../../assets/data/products';
+import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import {cartActions} from '../../redux/slices/cartSlice'
 const ProductCard = ({item}) => {
+
+    const dispatch = useDispatch()
+
+    const addToCart =()=>{
+        dispatch(cartActions.addItem({
+            id:item.id,
+            productName:item.productName,
+            price:item.price,
+            image:item.imgUrl,
+        }));
+        toast.success("Product added successfully");
+    }
   return (
         <Col lg='3' md='4' className='mb-2'>
         <div className="product__item">
@@ -19,7 +34,7 @@ const ProductCard = ({item}) => {
             <div className="product__card-bottom d-flex align-items-center
             justify-content-between p-2">
                 <span className="price">${item.price}</span>
-                <motion.span whileTap={{scale:1.2}}>
+                <motion.span whileTap={{scale:1.2}} onClick={addToCart}>
                     <i class="ri-add-line"></i>
                 </motion.span>
             </div>
